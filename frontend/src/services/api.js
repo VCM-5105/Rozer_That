6ec8843d86c50d75e377945f85535934ad448e90
Vipul Chandra Mishra <paritoshmishra5105@gmail.com>
2026-08-18 16:50:45 +0530

    updated api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || "/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("rozer_token");
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('rozer_token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default API;
